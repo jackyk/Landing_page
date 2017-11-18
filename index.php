dce4887e912925ddaedc1016cee3f35fef1f73e0
root <root@localhost.localdomain>
2017-11-18 13:40:25 +0300

    php design

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>First Page</title>
+    <style media="screen">
+      body{
+        background-image:url("footer_image_background_kenkhero.png");
+        background-repeat: no-repeat;
+        background-size: contain;
+        font-size: 100%;
+        padding: 0 !important;
+        margin: 0 !important;
+        font-family: "Raleway", "Helvetica Neue", Helvetica, Arial, sans-serif;
+      }
+
+      .lead{
+        text-align: left;
+        margin-left:15%;
+        font-size: 100px;
+        font-size: 9vw;
+        margin-top: 0 !important;
+        font-weight: 600;
+        }
+
+    .awesome {
+        background-color: #eeff00;
+        margin-left: 15%;
+        width: 20%;
+        height: 40%;
+        float:left;
+      }
+      .awesome h1{
+        margin-left:10%;  
+      }
+      .typo{
+        margin-left: 40%;
+        }
+      .typo h2{
+          float:left;
+        }
+    </style>
+  </head>
+  <body>
+
+    <div class="lead">LIVE. <br> DESIGN. <br> INSPIRE.</div>
+    <div class="awesome">
+      <h1>THE ROAD TO <br> <span style="color:#ffffff;">AWESOME :</span> </h1>
+    </div>
+    <div class="typo">
+      <p>Every Mistake - Every Rejection - Every <span style=" text-decoration:line-through;">taipo</span> typo -</p>
+      <h2>Has led me here. Who is this Awesome Guy?</h2 >
+      <a href="http://www.kenkhero.co.ke/avengers/"><img src="next_button_kenkhero.png" alt="click next" height="60px"width="50px"></a>
+    </div>
+
+
+  </body>
+</html>
